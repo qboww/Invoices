e9df26682dac48f4a61980688fb96ec1dc83967d
Evhenii Sarancha <y.i.sarancha@student.khai.edu>
2023-07-15 12:25:38 +0300

    added Authorization Policy

--- Pages/Invoices/Index.cshtml.cs
+++ Pages/Invoices/Index.cshtml.cs
@@ -7,9 +7,11 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using IdentityApp.Data;
 using IdentityApp.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace IdentityApp.Pages.Invoices
 {
+    [AllowAnonymous]
     public class IndexModel : PageModel
     {
         private readonly IdentityApp.Data.ApplicationDbContext _context;
