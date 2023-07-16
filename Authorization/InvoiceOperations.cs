5a9757c7de81160e229edb6731571901810e76ad
Evhenii Sarancha <y.i.sarancha@student.khai.edu>
2023-07-16 13:28:44 +0300

    added Invoice Manager Authorization Handler

--- Authorization/InvoiceOperations.cs
+++ Authorization/InvoiceOperations.cs
@@ -32,5 +32,7 @@ namespace IdentityApp.Authorization
 
         public static readonly string ApprovedOperationName = "Approved";
         public static readonly string RejectedOperationName = "Rejected";
+
+        public static readonly string InvoiceManagersRole = "InvoiceManager";
     }
 }
