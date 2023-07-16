2853dd910b3a432713cb5d595e8f1f41eb207bdf
Evhenii Sarancha <y.i.sarancha@student.khai.edu>
2023-07-16 18:13:23 +0300

    managed Chart total invoice revenue

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,20 +1,46 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using IdentityApp.Data;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace IdentityApp.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        public Dictionary<string, int> revenue;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        private readonly ILogger<IndexModel> _logger;
+        private readonly ApplicationDbContext _context;
+
+        public IndexModel(ILogger<IndexModel> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public void OnGet()
         {
+            revenue = new Dictionary<string, int>()
+            {
+                { "January", 0},
+                { "February", 0},
+                { "March", 0},
+                { "April", 0},
+                { "May", 0},
+                { "June", 0},
+                { "July", 0},
+                { "August", 0},
+                { "September", 0},
+                { "October", 0},
+                { "November", 0},
+                { "December", 0}
+            };
 
+            var invoices = _context.Invoice.ToList();
+
+            foreach (var invoice in invoices)
+            {
+                revenue[invoice.InvoiceMonth] += (int)invoice.InvoiceAmount;
+            }
         }
     }
 }
