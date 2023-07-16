762c68770ad67b42859d1cca3689405054eb42b8
Evhenii Sarancha <y.i.sarancha@student.khai.edu>
2023-07-16 18:25:09 +0300

    added graphs for all states

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -6,41 +6,68 @@ namespace IdentityApp.Pages
 {
     public class IndexModel : PageModel
     {
-        public Dictionary<string, int> revenue;
+
+        public Dictionary<string, int> revenueSubmitted;
+        public Dictionary<string, int> revenueApproved;
+        public Dictionary<string, int> revenueRejected;
 
         private readonly ILogger<IndexModel> _logger;
         private readonly ApplicationDbContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger, ApplicationDbContext context)
+        public IndexModel(ILogger<IndexModel> logger,
+            ApplicationDbContext context)
         {
             _logger = logger;
             _context = context;
         }
 
+
         public void OnGet()
         {
-            revenue = new Dictionary<string, int>()
-            {
-                { "January", 0},
-                { "February", 0},
-                { "March", 0},
-                { "April", 0},
-                { "May", 0},
-                { "June", 0},
-                { "July", 0},
-                { "August", 0},
-                { "September", 0},
-                { "October", 0},
-                { "November", 0},
-                { "December", 0}
-            };
+            InitDict(ref revenueSubmitted);
+            InitDict(ref revenueApproved);
+            InitDict(ref revenueRejected);
 
             var invoices = _context.Invoice.ToList();
 
             foreach (var invoice in invoices)
             {
-                revenue[invoice.InvoiceMonth] += (int)invoice.InvoiceAmount;
+
+                switch (invoice.Status)
+                {
+                    case InvoiceStatus.Submitted:
+                        revenueSubmitted[invoice.InvoiceMonth] += (int)invoice.InvoiceAmount;
+                        break;
+                    case InvoiceStatus.Approved:
+                        revenueApproved[invoice.InvoiceMonth] += (int)invoice.InvoiceAmount;
+                        break;
+                    case InvoiceStatus.Rejected:
+                        revenueRejected[invoice.InvoiceMonth] += (int)invoice.InvoiceAmount;
+                        break;
+                    default:
+                        break;
+                }
             }
+
+        }
+
+        private void InitDict(ref Dictionary<string, int> dict)
+        {
+            dict = new Dictionary<string, int>()
+            {
+                { "January",0 },
+                { "February",0 },
+                { "March",0 },
+                { "April",0 },
+                { "May",0 },
+                { "June",0 },
+                { "July",0 },
+                { "August",0 },
+                { "September",0 },
+                { "October",0 },
+                { "November",0 },
+                { "December",0 }
+            };
         }
     }
 }
