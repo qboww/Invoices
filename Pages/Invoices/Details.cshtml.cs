e7a501e90d33f5df7ca1da8958e89a0372690c4c
Evhenii Sarancha <y.i.sarancha@student.khai.edu>
2023-07-16 13:11:28 +0300

    added Details Invoice Authorisation

--- Pages/Invoices/Details.cshtml.cs
+++ Pages/Invoices/Details.cshtml.cs
@@ -7,36 +7,45 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using IdentityApp.Data;
 using IdentityApp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using IdentityApp.Authorization;
 
 namespace IdentityApp.Pages.Invoices
 {
-    public class DetailsModel : PageModel
+    public class DetailsModel : DI_BasePageModel
     {
-        private readonly IdentityApp.Data.ApplicationDbContext _context;
 
-        public DetailsModel(IdentityApp.Data.ApplicationDbContext context)
+        public DetailsModel(
+            ApplicationDbContext context,
+            IAuthorizationService authorizationService,
+            UserManager<IdentityUser> userManager)
+            : base(context, authorizationService, userManager)
         {
-            _context = context;
         }
 
-      public Invoice Invoice { get; set; } = default!; 
+        public Invoice Invoice { get; set; } 
 
         public async Task<IActionResult> OnGetAsync(int? id)
         {
-            if (id == null || _context.Invoice == null)
+            if (id == null)
             {
                 return NotFound();
             }
 
-            var invoice = await _context.Invoice.FirstOrDefaultAsync(m => m.InvoiceId == id);
-            if (invoice == null)
+            Invoice = await Context.Invoice.FirstOrDefaultAsync(m => m.InvoiceId == id);
+
+            if (Invoice == null)
             {
                 return NotFound();
             }
-            else 
-            {
-                Invoice = invoice;
-            }
+
+            var isAuthorized = await AuthorizationService.AuthorizeAsync(
+                User, Invoice, InvoiceOperations.Read);
+
+            if (isAuthorized.Succeeded == false)
+                return Forbid();
+
             return Page();
         }
     }
